180473ec00d346f547abb68196974aff4c3f2345
Rachel Schifano <rschifa1@gmail.com>
2016-05-16 23:46:03 -0500

    Add functions to an array and call them

--- Playgrounds/FirstClassDemo.playground/Contents.swift
+++ Playgrounds/FirstClassDemo.playground/Contents.swift
@@ -41,7 +41,27 @@ let b = {(x:Float, y:Float) -> Float
 // let abClosures = [a,b] : Will not compile because of the different types
 
 // Functions and Closures are the exact same thing, different syntax
+// Create functions, add them to array and call them
+func foo(x: Int) -> Int {
+    return 42 + x
+}
 
+let bar = {(x: Int) -> Int
+    in
+    42 + x
+}
 
+func curly(n: Int) -> Int {
+    return n * n
+}
 
+func larry(x: Int) -> Int {
+    return x * (x+1)
+}
 
+func moe(m: Int) -> Int {
+    return m * (m-1) * (m-2)
+}
+
+var stooges = [curly, larry, moe]
+stooges.append(bar)
